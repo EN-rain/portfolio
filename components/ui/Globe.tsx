46bfda529696d3eac34026fb5c32e68f5750f224
Ei <nieves.edriancit2011example.com>
2024-10-19 17:21:09 +0800

    hope

--- components/ui/Globe.tsx
+++ components/ui/Globe.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { Color, Scene, Fog, PerspectiveCamera, Vector3 } from "three";
 import ThreeGlobe from "three-globe";
 import { useThree, Object3DNode, Canvas, extend } from "@react-three/fiber";
@@ -98,9 +98,9 @@ export function Globe({ globeConfig, data }: WorldProps) {
     }
   }, [globeRef.current]);
 
-  const _buildMaterial = () => {
+  const _buildMaterial = useCallback(() => {
     if (!globeRef.current) return;
-
+  
     const globeMaterial = globeRef.current.globeMaterial() as unknown as {
       color: Color;
       emissive: Color;
@@ -111,9 +111,9 @@ export function Globe({ globeConfig, data }: WorldProps) {
     globeMaterial.emissive = new Color(globeConfig.emissive);
     globeMaterial.emissiveIntensity = globeConfig.emissiveIntensity || 0.1;
     globeMaterial.shininess = globeConfig.shininess || 0.9;
-  };
-
-  const _buildData = () => {
+  }, [globeConfig.globeColor, globeConfig.emissive, globeConfig.emissiveIntensity, globeConfig.shininess]);
+  
+  const _buildData = useCallback(() => {
     const arcs = data;
     let points = [];
     for (let i = 0; i < arcs.length; i++) {
@@ -134,8 +134,7 @@ export function Globe({ globeConfig, data }: WorldProps) {
         lng: arc.endLng,
       });
     }
-
-    // remove duplicates for same lat and lng
+  
     const filteredPoints = points.filter(
       (v, i, a) =>
         a.findIndex((v2) =>
@@ -144,9 +143,10 @@ export function Globe({ globeConfig, data }: WorldProps) {
           )
         ) === i
     );
-
+  
     setGlobeData(filteredPoints);
-  };
+  }, [data, defaultProps.pointSize]);
+  
 
   useEffect(() => {
     if (globeRef.current && globeData) {
